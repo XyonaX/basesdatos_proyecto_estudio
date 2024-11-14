08035f38fed3c800f9cf625f291df614ec1eec98
malenpinto <153029623+malenpinto@users.noreply.github.com>
2024-11-14 08:24:02 -0300

    Update script_insert_proyecto.sql

--- script/script_insert_proyecto.sql
+++ script/script_insert_proyecto.sql
@@ -2,7 +2,7 @@
 -- INSERCIÓN DEL LOTE DE DATOS
 
 USE StoredOps
-
+---------------------
 --- LOTE DE DATOS ---
 ---------------------
 --Paises
@@ -14,7 +14,7 @@ VALUES
 	('Paraguay'),
 	('Uruguay');
 
--- Provincias
+-- Provincias Argentina
 INSERT INTO Provincia (nombre_Provincia, id_Pais) 
 VALUES 
 	('Buenos Aires', 1),  
@@ -41,6 +41,7 @@ VALUES
 	('Tierra del Fuego', 1),
 	('Tucumán', 1);
 
+--Provincias Brasil	
 INSERT INTO Provincia (nombre_Provincia, id_Pais)
 VALUES 
 	('São Paulo', 2),
@@ -48,6 +49,7 @@ VALUES
 	('Minas Gerais', 2),
 	('Bahia', 2);
 
+--Provincias Paraguay
 INSERT INTO Provincia (nombre_Provincia, id_Pais)
 VALUES 
 	('Asunción', 3),
@@ -55,6 +57,7 @@ VALUES
 	('Alto Paraná', 3),
     	('Itapúa', 3);
 
+--Provincias Uruguay
 INSERT INTO Provincia (nombre_Provincia, id_Pais)
 VALUES
 	('Montevideo', 4),
@@ -242,4 +245,84 @@ VALUES
     ('Santiago de Liniers', 13),
     ('Tres Capones', 13),
     ('Wanda', 13);
+
+
+-- Tipos de Vehículo
+INSERT INTO Tipo_Vehiculo (nombre_tipoVehiculo) VALUES ('Sedan'), ('SUV'), ('Camioneta'), ('Hatchback'), ('Coupe');
+
+-- Vehículos
+INSERT INTO Vehiculos (marca_Vehiculo, modelo_Vehiculo, version_Vehiculo, km_Vehiculo, anio_Vehiculo, patente_Vehiculo, codigo_OKM, precio_Vehiculo, id_tipoVehiculo, ruta_imagen) 
+VALUES 
+    ('Toyota', 'Corolla', 'SE', 15000, '2021-01-01', 'ABC123', NULL, 25000, 1, '/images/toyota_corolla.png'),
+    ('Ford', 'Ecosport', 'Titanium', 30000, '2020-06-01', 'DEF456', NULL, 28000, 2, '/images/ford_ecosport.png'),
+    ('Chevrolet', 'S10', 'High Country', 40000, '2019-03-01', 'GHI789', NULL, 32000, 3, '/images/chevrolet_s10.png'),
+    ('Volkswagen', 'Golf', 'Comfortline', 20000, '2022-05-01', 'JKL012', NULL, 27000, 4, '/images/vw_golf.png'),
+    ('Honda', 'Civic', 'EX', 10000, '2021-09-01', 'MNO345', NULL, 24000, 1, '/images/honda_civic.png');
+
+-- Perfil del empleado
+INSERT INTO Perfil_Empleado (nombre_Perfil) 
+VALUES 
+    ('Administrador'),
+    ('Cajero'),
+    ('Vendedor');
+
+-- Insertar un Empleado 
+INSERT INTO Empleado (cuil_Empleado, dni_Empleado, nombre_Empleado, apellido_Empleado, email_Empleado, celular_Empleado, fechaNac_Empleado, calle_Empleado, num_Calle_Empleado, codigo_PostalEmpleado, id_Localidad, id_perfil) 
+VALUES ('20304050607', '12345678', 'Juan', 'Pérez', 'juan.perez@empresa.com', '123456789', '1980-01-01', 'Av. Siempreviva', 742, 5000, 1, 1);
+
+-- Insertar un cliente
+INSERT INTO Cliente (nombre_Cliente, apellido_Cliente, dni_Cliente, cuil_Cliente, fechaNac_Cliente, email_Cliente, celular_Cliente, calle_Cliente, num_Calle, piso_Cliente, dpto_Cliente, codigo_PostalCliente, id_Localidad) 
+VALUES ('Carlos', 'Gomez', '87654321', '20987654321', '1985-05-05', 'carlos.gomez@cliente.com', '987654321', 'Calle Falsa', 123, 1, 'A', 5001, 2);
+
+--Pedidos
+INSERT INTO Pedido (cuil_Empleado, id_Cliente, id_Vehiculo, fecha_Pedido, monto_Pedido, id_EstadoPedido) 
+VALUES 
+    ('20304050607', 1, 100, '2023-01-01', 25500, 1),
+    ('20304050607', 1, 101, '2023-01-05', 28000, 1),
+    ('20304050607', 1, 102, '2023-01-10', 32500, 1),
+    ('20304050607', 1, 103, '2023-02-01', 27500, 1),
+    ('20304050607', 1, 104, '2023-02-10', 24500, 1);
+
+-- Tipos de Pago
+INSERT INTO Tipo_Pago (nombre_tipoPago) VALUES ('Contado'), ('Tarjeta de Crédito'), ('Transferencia Bancaria');
+
+--Estados de Pago
+INSERT INTO Estado_Pago (nombre_EstadoPago) VALUES ('Pendiente'), ('Pagado'), ('Cancelado');
+
+-- Empleados
+INSERT INTO Empleado (cuil_Empleado, dni_Empleado, nombre_Empleado, apellido_Empleado, email_Empleado, celular_Empleado, fechaNac_Empleado, calle_Empleado, num_Calle_Empleado, piso_Empleado, dpto_Empleado, codigo_PostalEmpleado, id_Localidad, id_perfil) 
+VALUES 
+('20123456780', '12345578', 'María', 'Gómez', 'maria.gomez@empresa.com', '1234567890', '1985-04-25', 'Av. Siempre Viva', 123, NULL, NULL, 4000, 1, 1),
+('20234567891', '23456789', 'Juan', 'Pérez', 'juan.perez1@empresa.com', '0987654321', '1980-06-15', 'Calle Falsa', 456, 2, 'B', 5000, 2, 2),
+('20345678912', '34567891', 'Lucía', 'Ramírez', 'lucia.ramirez@empresa.com', '1122334455', '1992-03-10', 'Pje. del Sol', 789, 3, 'A', 3000, 3, 3),
+('20456789023', '45678912', 'Carlos', 'Martínez', 'carlos.martinez@empresa.com', '2233445566', '1988-11-05', 'Av. del Libertador', 120, NULL, NULL, 6000, 4, 1),
+('20567890134', '56789123', 'Ana', 'López', 'ana.lopez@empresa.com', '3344556677', '1975-12-20', 'Boulevard 23', 500, NULL, NULL, 7000, 5, 2);
+
+-- Pedidos
+INSERT INTO Pedido (cuil_Empleado, id_Cliente, id_Vehiculo, fecha_Pedido, monto_Pedido, id_EstadoPedido) 
+VALUES 
+('20123456780', 1, 100, GETDATE(), 22000.00, 1),
+('20234567891', 1, 101, GETDATE(), 18000.00, 2),
+('20345678912', 1, 102, GETDATE(), 25000.00, 3),
+('20456789023', 1, 103, GETDATE(), 17000.00, 4),
+('20567890134', 1, 104, GETDATE(), 30000.00, 1);
+
+-- Pagos
+INSERT INTO Pago (descripcion_Pago, importe_Pago, id_TipoPago, id_EstadoPago, id_Pedido) 
+VALUES 
+('Pago en efectivo', 18000.00, 2, 2, 8001),
+('Pago financiado', 25000.00, 1, 3, 8002),
+('Pago inicial', 17000.00, 2, 3, 8003),
+('Pago completo', 30000.00, 1, 1, 8004),
+('Pago inicial', 22000.00, 1, 1, 8005);
+
+-- Facturas
+INSERT INTO Factura (fecha_Factura, total_Factura, id_pago) 
+VALUES 
+(GETDATE(), 11000.00, 1),
+(GETDATE(), 18000.00, 2),
+(GETDATE(), 25000.00, 3),
+(GETDATE(), 17000.00, 4),
+(GETDATE(), 30000.00, 5);
+
 
